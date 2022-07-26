3a9f9358ee6d8fa6c09f074ba62a42984ece0467
Frolov <ilia.frolov@intive.com>
2022-07-26 12:01:22 +0300

    set up react-firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import React, { useState } from "react";
+import { FirebaseDatabaseProvider, FirebaseDatabaseNode } from "@react-firebase/database";
+import firebase from "firebase/app";
+import "firebase/database";
+import config from "./firebase-config";
 
 function App() {
+  const s = (a) => JSON.stringify(a, null, 2);
+  const [limit, setLimit] = useState(2);
+
   return (
     <div className="App">
-      Hello world
+      <FirebaseDatabaseProvider firebase={firebase} {...config}>
+        <FirebaseDatabaseNode
+          path="users/"
+          limitToFirst={limit}
+        >
+          {d => {
+            return (
+              <>
+                <pre>Path {d.path}</pre>
+                <pre style={{ height: 300, overflow: "auto" }}>
+                  Value {s(d.value)}
+                </pre>
+                <button
+                  onClick={() => {
+                    setLimit(limit + 2);
+                  }}
+                >
+                  Load more
+                </button>
+              </>
+            );
+          }}
+        </FirebaseDatabaseNode>
+      </FirebaseDatabaseProvider>
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlTk7Sv4Vjx8dNacbpGm7RHtmygAzYhTA",
+    authDomain: "gaze-analytic.firebaseapp.com",
+    databaseURL: "https://gaze-analytic-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gaze-analytic",
+    storageBucket: "gaze-analytic.appspot.com",
+    messagingSenderId: "646497392377",
+    appId: "1:646497392377:web:b109fd1cd87f634189f428",
+    measurementId: "G-V7RWG0FV1Q",
+};
+
+export default firebaseConfig;
